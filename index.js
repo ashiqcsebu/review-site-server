1bc32a3bca42a3bd122d76d6cd9c5114513a17b6
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-11-10 22:43:45 +0600

    unnecessary code ,removed and slighytly updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express() ;
 const port = process.env.PORT || 5000 ;
-const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
@@ -11,26 +11,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.plfe0ce.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-// function verifyJWT(req, res, next){
-//     const authHeader = req.headers.authorization;
-
-//     if(!authHeader){
-//         return res.status(401).send({message: 'unauthorized access'});
-//     }
-//     const token = authHeader.split(' ')[1];
-
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-//         if(err){
-//             return res.status(403).send({message: 'Forbidden access'});
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
-
-
 
 
 
@@ -76,7 +56,6 @@ app.post('/addreview' , async (req, res)=>{
 })
 
 
-
 app.get('/userreview', async (req,res)=>{
     let query = {}
     if(req.query.serviceName){
@@ -99,8 +78,6 @@ app.get('/userreview', async (req,res)=>{
 })
 
 
-
-
   app.get('/myreviw',   async (req,res)=>{
    
 
@@ -116,20 +93,14 @@ app.get('/userreview', async (req,res)=>{
   })
 
 
-
     }
     finally{
-
 
     }
 
 }
 
 run().catch(err => console.log(err));
-
-
-
-
 
 
 app.get('/' , (req , res)=>{
